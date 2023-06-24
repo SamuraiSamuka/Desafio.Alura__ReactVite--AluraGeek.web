ea8b17254fc58ca8511e43fd0388b9ca1b6b0efe
Samuel Silva de Carvalho <samuel.carvalho.dev@gmail.com>
2023-06-24 10:48:23 -0300

    Estilizando página de conta

--- src/Pages/Carrinho/Page.jsx
+++ src/Pages/Carrinho/Page.jsx
@@ -18,98 +18,103 @@ export async function action() {
 }
 
 export default function PaginaCarrinho() {
-    const {carrinho, totalCarrinho, limparCarrinho} = useContext(CarrinhoContext)
-    const {verificaSeLogado} = useContext(UsuarioContext)
-    const [formaPagamento, setFormaPagamento] = useState("")
-    const navigate = useNavigate();
-    const submit = useSubmit();
+  const {carrinho, totalCarrinho, limparCarrinho} = useContext(CarrinhoContext)
+  const {verificaSeLogado} = useContext(UsuarioContext)
+  const [formaPagamento, setFormaPagamento] = useState("")
+  const navigate = useNavigate();
+  const submit = useSubmit();
 
-    function submeteOuRedireciona(event) {
-        const form = event.target.form
-        if (verificaSeLogado()) {
-            if (form.checkValidity()) {
-                submit(form)
-                limparCarrinho()
-            } else {
-                form.reportValidity();
-            }
-        } else {
-            navigate('/login')
-        }
+  function submeteOuRedireciona(event) {
+    const form = event.target.form
+    if (verificaSeLogado()) {
+      if (form.checkValidity()) {
+        submit(form)
+        limparCarrinho()
+      } else {
+        form.reportValidity();
+      }
+    } else {
+      navigate('/login')
     }
+  }
 
-    return (
-        <main className="principal">
-            <div className="carrinho-container container">
-                <div className="carrinho-cabecalho">
-                    <NavLink to={-1}>
-                        <span className='carrinho__seta-back material-symbols-outlined'>arrow_back</span>
-                    </NavLink>
-                    <h2 className="carrinho__titulo">Carrinho</h2>
-                </div>
-                <div className="carrinho">
-                    <div className="carrinho__itens">
-                        {
-                        carrinho.length === 0 ? <div className="carrinho-vazio">
-                            <span className='carrinho-vazio__icone material-symbols-outlined'>shopping_cart</span>
-                            <p className='carrinho-vazio__mensagem'>O carrinho está vazio</p>
-                        </div> : carrinho.map((item) => {
-                            return (
-                                <ItemCarrinho key={
-                                        item.id
-                                    }
-                                    item={item}/>
-                            )
-                        })
-                    } </div>
-                    {
-                      carrinho.length === 0 ? "" : <hr className='carrinho__divisoria'/> 
-                    }{
-                    carrinho.length === 0 ? "" : <div className="carrinho-resumo">
-                        <Form method='post' className='carrinho-resumo__form'>
-                            <h3 className="carrinho-resumo__titulo">Resumo</h3>
-                            <CampoInput type="select"
-                                opcoesLista={
-                                    ["Cartão de crédito", "Cartão de débito", "Pix", "Boleto"]
-                                }
-                                valor={formaPagamento}
-                                required
-                                aoAlterado={
-                                    (e) => {
-                                        setFormaPagamento(e.target.value)
-                                    }
-                            }>Forma de pagamento</CampoInput>
-                            {
-                            formaPagamento === 'Cartão de crédito' ? <CampoInput type="select" required
-                                opcoesLista={
-                                    [
-                                        "1x sem juros",
-                                        "2x sem juros",
-                                        "3x sem juros",
-                                        "4x sem juros",
-                                        "5x sem juros",
-                                        "6x sem juros",
-                                        "7x sem juros",
-                                        "8x sem juros",
-                                        "9x sem juros",
-                                        "10x sem juros",
-                                        "11x sem juros",
-                                        "12x sem juros"
-                                    ]
-                            }>Parcelas</CampoInput> : formaPagamento === "" ? <p className='carrinho__forma-pagamento-parcela'>Escolha a forma de pagamento</p> : <p className='carrinho__forma-pagamento-parcela'>Pagamento à vista</p>
-                        }
-                            <h4 className='carrinho__total'>Total R$ {
-                                totalCarrinho.toFixed(2)
-                            }</h4>
-                            <Botao type="button"
-                                onClick={submeteOuRedireciona}
-                                disabled={
-                                    carrinho.length === 0 ? "disabled" : ""
-                            }>Finalizar pedido</Botao>
-                        </Form>
-                    </div>
-                } </div>
-            </div>
-        </main>
-    )
+  return (
+    <main className="principal">
+      <div className="carrinho-container container">
+        <div className="carrinho-cabecalho">
+          <NavLink to={-1}>
+            <span className='carrinho__seta-back material-symbols-outlined'>arrow_back</span>
+          </NavLink>
+          <h2 className="carrinho__titulo">Carrinho</h2>
+        </div>
+        <div className="carrinho">
+          <div className="carrinho__itens">
+              {
+                carrinho.length === 0 ? 
+                  <div className="carrinho-vazio">
+                    <span className='carrinho-vazio__icone material-symbols-outlined'>shopping_cart</span>
+                    <p className='carrinho-vazio__mensagem'>O carrinho está vazio</p>
+                  </div>
+                : carrinho.map((item) => {
+                  return (
+                    <ItemCarrinho 
+                      key={ item.id }
+                      item={item}/> )
+                  })
+              }
+          </div>
+          {
+            carrinho.length === 0 ? "" : <hr className='carrinho__divisoria'/> 
+          }
+          {
+            carrinho.length === 0 ? "" 
+            : <div className="carrinho-resumo">
+                <Form method='post' className='carrinho-resumo__form'>
+                  <h3 className="carrinho-resumo__titulo">Resumo</h3>
+
+                  <CampoInput 
+                    type="select"
+                    opcoesLista={
+                        ["Cartão de crédito", "Cartão de débito", "Pix", "Boleto"]
+                    }
+                    valor={formaPagamento}
+                    required
+                    aoAlterado={ (e) => { setFormaPagamento(e.target.value) }}
+                  >Forma de pagamento</CampoInput>
+
+                  {
+                    formaPagamento === 'Cartão de crédito' ? 
+                      <CampoInput type="select" required
+                        opcoesLista={[
+                            "1x sem juros",
+                            "2x sem juros",
+                            "3x sem juros",
+                            "4x sem juros",
+                            "5x sem juros",
+                            "6x sem juros",
+                            "7x sem juros",
+                            "8x sem juros",
+                            "9x sem juros",
+                            "10x sem juros",
+                            "11x sem juros",
+                            "12x sem juros"
+                          ]}
+                        >Parcelas</CampoInput> 
+                    : formaPagamento === "" ? 
+                        <p className='carrinho__forma-pagamento-parcela'>Escolha a forma de pagamento</p> 
+                      : <p className='carrinho__forma-pagamento-parcela'>Pagamento à vista</p>
+                  }
+                  <h4 className='carrinho__total'>Total R$ { totalCarrinho.toFixed(2) }</h4>
+                  <Botao 
+                    type="button"
+                    onClick={submeteOuRedireciona}
+                    disabled={ carrinho.length === 0 ? "disabled" : "" }
+                  >Finalizar pedido</Botao>
+                </Form>
+              </div>
+          } 
+        </div>
+      </div>
+    </main>
+  )
 }
--- src/Pages/Conta/Page.jsx
+++ src/Pages/Conta/Page.jsx
@@ -2,6 +2,7 @@ import { NavLink } from 'react-router-dom'
 import './Conta.css'
 import { useContext } from 'react'
 import { UsuarioContext } from '../../common/Usuario'
+import imagePerfil from '/images/perfil.jpeg'
 
 export default function PaginaConta() {
   const { usuario, deslogar } = useContext(UsuarioContext)
@@ -10,15 +11,17 @@ export default function PaginaConta() {
     <main className="principal">
         <div className="conta__container container">
           <div className="conta__side-bar">
-            <img src={usuario.imagem_src} alt="" className="conta__imagem" />
+            <img src={imagePerfil} alt="" className="conta__imagem" />
             <nav className="conta__opcoes">
-              <NavLink>Perfil</NavLink>
-              <NavLink>Pedidos</NavLink>
-              <NavLink>Logout</NavLink>
+              <NavLink className="conta__opcao">Perfil</NavLink>
+              <NavLink className="conta__opcao">Pedidos</NavLink>
+              <button className="perfil__logout"  onClick={deslogar}>
+                <span className="perfil__logout-botao material-symbols-outlined">logout</span>
+                <p className="perfil__logout-texto">Sair</p>
+              </button>
             </nav>
           </div>
           <div className="conta__content"></div>
-            <span className="perfil__logout material-symbols-outlined" onClick={deslogar} >logout</span>
         </div>
     </main>
   )
